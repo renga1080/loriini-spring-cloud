35c03a481c5a14775863c030e3a843623179b2b4
Rengarajan.Kannan <rengarajan.kannan@cognizant.com>
2019-03-24 15:19:17 -0400

    First commit

---
+++ src/main/java/com/loriini/cloud/LoriiniSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.loriini.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoriiniSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoriiniSpringCloudApplication.class, args);
+	}
+
+}
