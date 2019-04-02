8511d84a9a0d2f7f94e08e449b4c9366c522fd55
Rengarajan.Kannan <rengarajan.kannan@cognizant.com>
2019-04-01 22:30:24 -0400

    config server config

--- loriini-config-server/src/main/java/com/loriini/cloud/config/LoriiniConfigServerApplication.java
+++ loriini-config-server/src/main/java/com/loriini/cloud/config/LoriiniConfigServerApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableConfigServer
 public class LoriiniConfigServerApplication {
 
     public static void main(String[] args) {
